dbcb73ddde9dabbda92aaf3440ea1141ba46161c
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-08-05 17:20:17 +0300

    Connect About Center management and contact pages

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
+
 Route::view('/pro-tsentr', 'pages.about')->name('about');
-Route::view('/pro-tsentr/upravlinnia', 'pages.placeholder', ['title' => 'Управління'])->name('about.management');
-Route::view('/pro-tsentr/entsyklopediia', 'pages.placeholder', ['title' => 'Енциклопедія'])->name('about.encyclopedia');
+Route::view('/pro-tsentr/upravlinnia', 'pages.management')->name('about.management');
+Route::redirect('/pro-tsentr/entsyklopediia', 'https://wiki.svit.in.ua/', 302)->name('about.encyclopedia');
+Route::view('/zviazok', 'pages.contact')->name('contact');
 
 foreach ([
     'ridna-vira' => 'Рідна Віра',
@@ -13,7 +15,6 @@ foreach ([
     'statti' => 'Статті',
     'tvorchist' => 'Творчість',
     'kramnychka' => 'Крамниця',
-    'zviazok' => 'Зв’язок',
     'koshyk' => 'Кошик',
 ] as $slug => $title) {
     Route::view('/'.$slug, 'pages.placeholder', compact('title'));
