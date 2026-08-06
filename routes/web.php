99db6c8d72c4bad8c4cd050b0876fd922f456045
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-08-06 22:03:16 +0300

    Register formal contact form route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactFormController;
 use App\Http\Controllers\DocumentDownloadController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,9 @@ Route::view('/pro-tsentr', 'pages.about')->name('about');
 Route::view('/pro-tsentr/upravlinnia', 'pages.management')->name('about.management');
 Route::redirect('/pro-tsentr/entsyklopediia', 'https://wiki.svit.in.ua/', 302)->name('about.encyclopedia');
 Route::view('/zviazok', 'pages.contact')->name('contact');
+Route::post('/zviazok', ContactFormController::class)
+    ->middleware('throttle:5,1')
+    ->name('contact.submit');
 
 Route::get('/dokumenti/{document}', DocumentDownloadController::class)
     ->where('document', '[a-z0-9\-]+')
