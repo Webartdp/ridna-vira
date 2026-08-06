c3e28dc8e88f9dfdb7102df4b19127a122e24ead
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-08-06 20:20:43 +0300

    Add local document download route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DocumentDownloadController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
@@ -8,6 +9,10 @@ Route::view('/pro-tsentr', 'pages.about')->name('about');
 Route::view('/pro-tsentr/upravlinnia', 'pages.management')->name('about.management');
 Route::redirect('/pro-tsentr/entsyklopediia', 'https://wiki.svit.in.ua/', 302)->name('about.encyclopedia');
 Route::view('/zviazok', 'pages.contact')->name('contact');
+
+Route::get('/dokumenti/{document}', DocumentDownloadController::class)
+    ->where('document', '[a-z0-9\-]+')
+    ->name('documents.download');
 
 foreach ([
     'ridna-vira' => 'Рідна Віра',
