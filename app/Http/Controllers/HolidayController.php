a64bed4f937b8dda9b68415b25e9321a815660e6
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-08-21 12:47:35 +0300

    Use local source-backed holiday descriptions

--- app/Http/Controllers/HolidayController.php
+++ app/Http/Controllers/HolidayController.php
@@ -12,15 +12,19 @@ final class HolidayController extends Controller
     public function __invoke(string $holiday): View
     {
         $calendar = config('faith_calendar');
+        $descriptions = config('faith_holidays', []);
         $months = $calendar['months'] ?? [];
 
         foreach ($months as $month => $items) {
             foreach ($items as $item) {
-                $slug = (string) ($item['slug'] ?? Str::slug((string) ($item['name'] ?? '')));
+                $name = (string) ($item['name'] ?? '');
+                $slug = (string) ($item['slug'] ?? Str::slug($name));
 
                 if ($slug !== $holiday) {
                     continue;
                 }
+
+                $item['description'] = (string) ($item['description'] ?? ($descriptions[$name] ?? ''));
 
                 $contentPath = resource_path('content/holidays/'.$slug.'.html');
                 $content = is_file($contentPath) ? (string) file_get_contents($contentPath) : null;
