7793c973c97ccd600009cb5c13fe7d2683a36367
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-08-21 12:48:15 +0300

    Load holiday page styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,7 @@ import '../scss/contact-form.scss';
 import '../scss/navigation.scss';
 import '../scss/faith.scss';
 import '../scss/faith-calendar.scss';
+import '../scss/holiday.scss';
 import './navigation.js';
 import './contact-map.js';
 import './contact-form.js';
