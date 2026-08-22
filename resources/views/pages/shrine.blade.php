7a310938f462be25a2ebf8997c2bc4fc506a8317
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-08-22 22:19:53 +0300

    Ignore legacy GIF assets on shrine pages

--- resources/views/pages/shrine.blade.php
+++ resources/views/pages/shrine.blade.php
@@ -9,7 +9,7 @@
     foreach (($shrine['assets'] ?? []) as $asset) {
         $path = (string) ($asset['path'] ?? '');
 
-        if ($path !== '' && preg_match('/\.(?:jpe?g|png|gif|webp|svg)(?:\?.*)?$/i', $path)) {
+        if ($path !== '' && preg_match('/\.(?:jpe?g|png|webp|svg)(?:\?.*)?$/i', $path)) {
             $heroImage = $path;
             break;
         }
